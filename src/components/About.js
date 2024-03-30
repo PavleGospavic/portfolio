e05343a96434b5a6bf19fc036869a3e37787192a
Pavle Gospavic <pavlonek97@gmail.com>
2024-03-30 20:27:30 +0100

    Image directory changed

--- src/components/About.js
+++ src/components/About.js
@@ -30,7 +30,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="../../public/laptop.png"
+            src="./laptop.png"
           />
         </div>
       </div>
